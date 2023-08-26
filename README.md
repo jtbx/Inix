8cf49fc3e29bd9652bd0f54ac10ebebd7d648bbb
Jeremy <jtbx@duck.com>
2023-08-27 07:53:03 +1200

    Improve documentation
    Improved docs in
     - Documentation.txt
     - inix.h
     - README.md

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Inix
 Inix is a small and handy C library to handle and parse INI files.
-# How to setup Inix
-* Inix is composed of a C source file and a C header file, the whole library is fully coded in C99 compliant code.
-* To use Inix, simply drag the header file in your C project as well as the C source code file.
-* By default "inix.c" will search for the header file in the same folder.
-* If the header file is not in the same folder, please change the "#include" path in the "inix.c" file.
-* Finally to use the library simply "#include" the header file in your project.
-# How does the library work
-A full and detailed documentation is available in the Github repository.
-# Will Inix evolve
-The current version of the Inix library is the 1.0, the library is still at a very early stage and bugs are to be expected.
-However i will keep working on the project, fixing bugs and adding new features to the library, so stay tuned.
+
+## How to setup Inix
+* Inix is composed of a C source file and a header file. The whole library is programmed fully in C99 compliant code.
+* To use Inix, simply copy the header file and the source file into your C project.
+* By default inix.c will look for inix.h in the same folder.
+* If the header file is not in the same folder, add a `-Ipath` flag to Inix's compile command.
+* To use the library, simply #include the header file in your project.
+
+## Documentation
+Detailed documentation is available in Documentation.txt.
+
+## Will Inix evolve
+Inix is currently at version 1.0; the library is still at a very early stage and bugs are to be expected.
+However I will keep working on the project, fixing bugs and adding new features to the library, so stay tuned.
--- inix.h
+++ inix.h
@@ -1,15 +1,17 @@
 #ifndef INIX_H
 #define INIX_H
 
-/**************************************************************************************************
+/******************************************************************
 *
-* The Inix library is a free and open source INI parser C library delivered under the GPL3 license.
+* Inix: a free and open source INI parser C library licensed under
+* the GNU General Public License, version 3.
 *
-* The Inix library is a C library that will allow you to parse INI files.
-* It is coded in pure C99 with no compiler extensions, only using C standard libraries.
-* For more info about how the parser works, please checkout the doucmentation.
+* Inix is a C library that will allow you to parse INI files.
+* It is coded in pure C99 with no compiler extensions,
+* only using C standard libraries. For more info about how
+* the parser works, please checkout the documentation.
 *
-***************************************************************************************************/
+******************************************************************/
 
 
 
@@ -17,14 +19,14 @@
 * Inix macros
 */
 
-#define INIX_VERSION 1.0;           /* The verion of the library */
-#define INIX_AUTHOR "PINGY BOY"     /* The author of the library */
-#define INIX_LICENSE "GPL3"         /* The license of the library */
+#define INIX_VERSION 1.0
+#define INIX_AUTHOR "PINGY BOY"
+#define INIX_LICENSE "GPL3"
 
 #define BUFFER_SIZE 200             /* Defines the maximum buffer size when parsing lines */
 
 /*
-* INI structure to store and write INI files
+* Structures to store INI files
 */
 
 typedef struct IniElement {     /* The elements storing a key and a value */
@@ -37,49 +39,50 @@ typedef struct IniSection {     /* The sections storing an array of elements and
     IniElement  *elems;
 } IniSection;
 
-typedef struct IniStruct {      /* The main INIstruct storing an array of sections and a name */
+typedef struct IniStruct {      /* The main IniStruct storing an array of sections and a name */
     char            name[32];
     IniSection      *sects;
 } IniStruct;
 
 /*
-* Functions Prototypes to manage an INIstruct
+* Functions prototypes for manipulating an IniStruct
 */
 
-/* Core functions, Create, Reset and Free INIstructs */
-IniStruct *IniMake(char *);     /* Creates an empty INIstruct and return it's address */
-void IniEnd(IniStruct *);       /* Takes a pointer to an INIstruct and free it */
-void IniClear(IniStruct *);     /* Takes a pointer to an INIstruct and reset it */
+/* Core functions (create, reset and free IniStructs) */
+IniStruct *IniMake(char *);     /* Returns a pointer to a new IniStruct */
+void IniEnd(IniStruct *);       /* Frees the given IniStruct */
+void IniClear(IniStruct *);     /* Resets the given IniStruct */
 
-/* Input/Output functions, read from/write to an INIstruct */
-int IniAddSect(IniStruct *, char *);                        /* Adds a section to a given INIstruct */
-int IniAddElem(IniStruct *, char *, char *, char *);        /* Adds an element to a given section */
-int IniDelSect(IniStruct *, char *);                        /* Deletes a section from a given INIstruct */
-int IniDelElem(IniStruct *, char *, char *);                /* Deletes an element form a given section */
-int IniModSect(IniStruct *, char *, char *);                /* Modifies the name of a given section */
-int IniModElemValue(IniStruct *, char *, char *, char *);   /* Modifies the key of a given element in a given section */
-int IniModElemKey(IniStruct *, char *, char *, char *);     /* Modifies the value of a given elemnt in a given section */
-char *IniReadElem(IniStruct *, char *, char*);              /* Returns the value from a given elements in a given section */
+/* Input/Output functions */
+int   IniAddSect(IniStruct *, char *);                        /* Adds a section to an IniStruct */
+int   IniAddElem(IniStruct *, char *, char *, char *);        /* Adds an element to a given section */
+int   IniDelSect(IniStruct *, char *);                        /* Deletes a section from an IniStruct */
+int   IniDelElem(IniStruct *, char *, char *);                /* Deletes an element from a given section */
+int   IniModSect(IniStruct *, char *, char *);                /* Modifies the name of a given section */
+int   IniModElemValue(IniStruct *, char *, char *, char *);   /* Modifies the key of a given element in a given section */
+int   IniModElemKey(IniStruct *, char *, char *, char *);     /* Modifies the value of a given element in a given section */
+char *IniReadElem(IniStruct *, char *, char *);              /* Returns the value of an element in a given section */
 
-/* Listing functions, Lists everything in the whole struct or in a specific section */
-void IniListAll(IniStruct *);           /* List all elements and sections in a given INIstruct */
-int IniListSect(IniStruct *, char *);   /* List all elements of a given struct */
+/* Listing functions (lists everything in the whole struct or in a specific section) */
+void IniListAll(IniStruct *);           /* List all elements and sections in a given IniStruct */
+int  IniListSect(IniStruct *, char *);   /* List all elements of a given section */
 
-/* Seek utils functions, return true/false */
-int IniSeekSect(IniStruct *, char *);              /* Checks if a given section exists in a given INIstruct */
-int IniSeekElem(IniStruct *, char *, char *);      /* Checks if a given key exists in a given section in a given INIstruct */
+/* Seek utils functions (returns 1 on success, otherwise 0) */
+int IniSeekSect(IniStruct *, char *);              /* Checks if a given section exists in a given IniStruct */
+int IniSeekElem(IniStruct *, char *, char *);      /* Checks if a given key exists in a given section in a given IniStruct */
 
-/* Global section functions, utilities to handle the global part of an INIstruct */
-int IniAddGlob(IniStruct *, char *, char *);        /* Adds a global element */
-int IniDelGlob(IniStruct *, char *);                /* Deletes a global element */
-int IniModGlobKey(IniStruct *, char *, char *);     /* Modifies a global element's key */
-int IniModGlobValue(IniStruct *, char *, char *);   /* Modifies a global element's value */
-char *IniReadGlob(IniStruct *, char*);              /* Reads from a global element's and returns it's value */
-int IniListGlob(IniStruct *);                       /* Lists all elements of the global section */
-int IniSeekGlob(IniStruct *, char *);               /* Seeks an elements in a given section and returns true if it finds it */
+/* Global section functions (utilities to handle the global part of an IniStruct) */
+int   IniAddGlob(IniStruct *, char *, char *);      /* Adds a global element */
+int   IniDelGlob(IniStruct *, char *);              /* Deletes a global element */
+int   IniModGlobKey(IniStruct *, char *, char *);   /* Modifies a global element's key */
+int   IniModGlobValue(IniStruct *, char *, char *); /* Modifies a global element's value */
+char *IniReadGlob(IniStruct *, char*);              /* Returns the value of a global element */
+int   IniListGlob(IniStruct *);                     /* Lists all elements of the global section */
+int   IniSeekGlob(IniStruct *, char *);             /* Returns 1 if the given element exists in
+													   the global section, otherwise 0 */
 
 /* File functions, read from/write to an INI file */
-IniStruct *IniReadFile(char *, char *);     /* Reads from a file and puts the data in an INIstruct and returns the adress of it */
-void IniWriteFile(IniStruct *, char *);     /* Writes the content of an INIstruct into an INIfile */
+IniStruct *IniReadFile(char *, char *);     /* Reads the INI data from a file and returns a new IniStruct containing that data */
+void IniWriteFile(IniStruct *, char *);     /* Writes the content of an IniStruct into an INI file */
 
 #endif
